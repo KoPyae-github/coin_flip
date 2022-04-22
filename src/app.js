e18c990021e9a020f92837999348478954cfd6a8
KoPyae-github <pyae.phyo.zaw.me@gmail.com>
2022-04-22 12:38:29 +0630

    Change Port

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { showResult } = require('./flip');
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get("/games/coin-flip", (req, res) => {
     if (!req.query.pCoin) {
@@ -20,7 +21,6 @@ app.get("/games/coin-flip", (req, res) => {
     res.send(showResult(pCoin, bet));
 });
 
-let portNum = 4000;
-app.listen(portNum, () => {
-    console.log(`Server is running at port ${portNum}`);
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 })  
